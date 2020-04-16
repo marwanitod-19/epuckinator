30d3e170bd6c5d980f5b59fced688de5e147de86
FaFyle <49496148+FaFyle@users.noreply.github.com>
2020-04-16 15:57:14 +0200

    Added make_jump function.

--- e-cat/moves.c
+++ e-cat/moves.c
@@ -12,8 +12,8 @@
 #include <chprintf.h>
 
 #define CMtoSTEP	77.922 // 1 cm/s -> 77.922 step/s
-#define NB_MOVES	4
-enum moves {pause, stroll_move, look_around, circle_move};
+#define NB_MOVES	5
+enum moves {pause, stroll_move, look_around, circle_move, jump};
 //
 //int _gettimeofday( struct timeval *tv, void *tzvp )
 //{
@@ -30,30 +30,45 @@ int randomizer(int nb_rand){
 	return action;
 }
 
-//Casual stroll in a given direction
+//Casual stroll in a given direction.
 void stroll(int left_speed, int right_speed){
 	right_motor_set_speed(left_speed*CMtoSTEP);
 	left_motor_set_speed(right_speed*CMtoSTEP);
 
 }
 
-//By default, rotates to its right for positive speeds
+//By default, rotates to its right for positive speeds.
 void rotator(int speed){
 	stroll(speed,-speed);
 }
 
-
+//Stops motors until other function is called.
 void make_pause(void){
 	right_motor_set_speed(0);
 	left_motor_set_speed(0);
 }
 
-void make_circle(int size, int speed){
-	right_motor_set_speed(speed*CMtoSTEP);
-	left_motor_set_speed((speed + size)*CMtoSTEP);
+//Stops motors and ads a Thread sleep delay in ms.
+void pause_until(int ms_time){
+	make_pause();
+	chThdSleep(MS2ST(ms_time));
 }
 
-void make_look_around(void){		//regarde à gauche et a droite et choisit entre partir à gauche, à droite ou au milieu.
+//Makes an arc like move, random on which side is took.
+void make_circle(int size, int speed){
+	int direction = randomizer(1);
+	if (direction == 0){
+		right_motor_set_speed(speed*CMtoSTEP);
+		left_motor_set_speed((speed + size)*CMtoSTEP);
+	}
+	else{
+		left_motor_set_speed(speed*CMtoSTEP);
+		right_motor_set_speed((speed + size)*CMtoSTEP);
+	}
+}
+
+//Looks at left and right side and chooses randomly between going left, right or straight.
+void make_look_around(void){
 	int direction = randomizer(2);
 	rotator(-8);
 	chThdSleep(MS2ST(500));
@@ -75,6 +90,19 @@ void make_look_around(void){		//regarde à gauche et a droite et choisit entre pa
 	}
 }
 
+//Simulated "jump" by just letting e-cat do jump-like moves.
+//Make it look around first and choose where to jump randomly.
+void make_jump(void){
+	make_look_around();
+	stroll(15,15);
+	chThdSleep(MS2ST(200));
+	pause_until(300);
+	stroll(15,15);
+	chThdSleep(MS2ST(200));
+	pause_until(300);
+
+}
+
 static THD_WORKING_AREA(waMover, 256);
 static THD_FUNCTION(Mover, arg) {
 	chRegSetThreadName(__FUNCTION__);
@@ -84,17 +112,20 @@ static THD_FUNCTION(Mover, arg) {
 	while(1){
 		//time = chVTGetSystemTime();
 		// Condition qui choisit entre rotation et stroll?
-		action = look_around; //randomizer(NB_MOVES);
+		action = randomizer(NB_MOVES);
 		chprintf((BaseSequentialStream *)&SD3, "Action == %d \n", action);
 		if(action == pause)
 			make_pause();
 		if(action == stroll_move)
 			stroll(5,5);
-		if(action == look_around)
+		if(action == look_around){
 			make_look_around();
 			make_pause();
+		}
 		if(action == circle_move)
 			make_circle(3, 3);
+		if(action == jump)
+			make_jump();
 		chThdSleep(MS2ST(1000));
 	}
 
