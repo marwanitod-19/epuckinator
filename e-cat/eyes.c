6ef4dfb3cfbf45ddfd17d2f14f9859583ab085a0
Marwan El Chazli <marwan.elchazli@epfl.ch>
2020-05-04 17:33:41 +0200

    cleaned up eyes module

--- e-cat/eyes.c
+++ e-cat/eyes.c
@@ -52,129 +52,109 @@ static THD_FUNCTION(Eyes, arg) {
 	//VL53L0X_start();
 
 	while(1){
-			highest_prox = NO_OBSTACLES;
-			for(int i = 0 ; i < 8 ; i++){
-				inv_distance = get_calibrated_prox(i);
-				chprintf((BaseSequentialStream *)&SD3, "Sensor %d sees an object at distance %d \n\r", i, get_calibrated_prox(i));
-				if(inv_distance > OBST_THRESHOLD){
-					sat_sensor[i] = 1;
-					highest_prox = prox0;
-					mv_in_progress = true;
-					//chThdWait(get_thd_ptr());
-					obst_move = 1;
-					if(i != 0 && get_calibrated_prox(i-1) < inv_distance){
-						highest_prox = i;
-						sensor_count++;
-					}
+		highest_prox = NO_OBSTACLES;
+		for(int i = 0 ; i < 8 ; i++){
+			inv_distance = get_calibrated_prox(i);
+			chprintf((BaseSequentialStream *)&SD3, "Sensor %d sees an object at distance %d \n\r", i, get_calibrated_prox(i));
+			if(inv_distance > OBST_THRESHOLD){
+				sat_sensor[i] = 1;
+				highest_prox = prox0;
+				mv_in_progress = true;
+				//chThdWait(get_thd_ptr());
+				obst_move = 1;
+				if(i != 0 && get_calibrated_prox(i-1) < inv_distance){
+					highest_prox = i;
+					sensor_count++;
 				}
 			}
-			if(get_selector() != 1)
-			{
-				switch(highest_prox){
-					case prox0:
-						reaction(8, 1000);
-						break;
-					case prox1:
-						reaction(8, 800);
-						break;
-					case prox2:
-						reaction(8, 500);
-						break;
-					case prox5:
-						reaction(-8, 500);
-						break;
-					case prox6:
-						reaction(-8, 800);
-						break;
-					case prox7:
-						reaction(-8, 1000);
-						break;
-					case NO_OBSTACLES:
-						break;
-				}
-
-				if(highest_prox == 3 || highest_prox == 4 ){
-					if(sat_sensor[3] == 1 && sat_sensor[4] == 1){
-						chprintf((BaseSequentialStream *)&SD3, "Meow \n\r");
-						stroll(15, 15);
-						meow();
-						chThdSleep(MS2ST(200));
-					}
-				}
-				sensor_count = 0;
-				//chprintf((BaseSequentialStream *)&SD3, "The Closest distance is at sensor %d and the distance is %d \n\r\r", highest_prox, get_calibrated_prox(highest_prox));
-				if(mv_in_progress){
-					chThdSleep(MS2ST(1000));
-					mv_in_progress = false;
-				}
-				else
-					chThdSleep(MS2ST(100));
+		}
+		if(get_selector() == 1)
+		{
+			switch(highest_prox){
+				case prox0:
+					reaction(8, 1000);
+					break;
+				case prox1:
+					reaction(8, 800);
+					break;
+				case prox2:
+					reaction(8, 500);
+					break;
+				case prox5:
+					reaction(-8, 500);
+					break;
+				case prox6:
+					reaction(-8, 800);
+					break;
+				case prox7:
+					reaction(-8, 1000);
+					break;
+				case NO_OBSTACLES:
+					break;
 			}
 
-			else{
-				switch(highest_prox){
-					case prox0:
-						rotator(-8);
-						chThdSleep(MS2ST(200));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox1:
-						rotator(-8);
-						chThdSleep(MS2ST(500));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox2:
-						rotator(-8);
-						chThdSleep(MS2ST(800));
-						stroll(5,5);
-						purr();
-						chThdSleep(MS2ST(500));
-						break;
-					case prox3:
-						rotator(-8);
-						chThdSleep(MS2ST(1000));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox4://other side
-						rotator(8);
-						chThdSleep(MS2ST(1000));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox5:
-						rotator(8);
-						chThdSleep(MS2ST(800));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox6:
-						rotator(8);
-						chThdSleep(MS2ST(500));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case prox7:
-						rotator(8);
-						chThdSleep(MS2ST(200));
-						stroll(5,5);
-						chThdSleep(MS2ST(500));
-						break;
-					case NO_OBSTACLES:
-						break;
+			if(highest_prox == 3 || highest_prox == 4 ){
+				if(sat_sensor[3] == 1 && sat_sensor[4] == 1){
+					chprintf((BaseSequentialStream *)&SD3, "Meow \n\r");
+					stroll(15, 15);
+					meow();
+					chThdSleep(MS2ST(200));
 				}
-				sensor_count = 0;
-				if(mv_in_progress){
-					//chThdSleep(MS2ST(1000));
-					mv_in_progress = false;
-				}
-				else
-					chThdSleep(MS2ST(100));
-				//chThdSleep(MS2ST(3000));
 			}
-				obst_move = 0;
+			sensor_count = 0;
+			//chprintf((BaseSequentialStream *)&SD3, "The Closest distance is at sensor %d and the distance is %d \n\r\r", highest_prox, get_calibrated_prox(highest_prox));
+			if(mv_in_progress){
+				chThdSleep(MS2ST(1000));
+				mv_in_progress = false;
+			}
+			else
+				chThdSleep(MS2ST(100));
+		}
+
+		else if(get_selector == 2){
+			switch(highest_prox){
+				case prox0:
+					reaction(-8, 200);
+					break;
+				case prox1:
+					reaction(-8, 500);
+					break;
+				case prox2:
+					reaction(-8, 800);
+					break;
+				case prox3:
+					reaction(-8, 1000);
+					break;
+				case prox4://other side
+					reaction(8, 1000);
+					break;
+				case prox5:
+					reaction(8, 800);
+					break;
+				case prox6:
+					reaction(8, 500);
+					break;
+				case prox7:
+					reaction(8, 200);
+					break;
+				case NO_OBSTACLES:
+					break;
+			}
+			//chThdSleep(MS2ST(3000));
+		}
+
+		else{
+			chThdSleep(MS2ST(1000));
+		}
+		sensor_count = 0;
+		if(mv_in_progress){
+			//chThdSleep(MS2ST(1000));
+			mv_in_progress = false;
+		}
+		else{
+			chThdSleep(MS2ST(100));
+		}
+		obst_move = 0;
 	}
 }
 
